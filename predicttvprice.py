db895c18b4442c2249ed18fc103f8d6607788ef8
con266667 <cnwilson29@gmail.com>
2019-01-21 17:57:37 -0500

    added user input

--- predicttvprice.py
+++ predicttvprice.py
@@ -7,4 +7,6 @@ features = [[1080, 43], [720, 32], [1080, 49], [3840, 55], [3840, 42], [3840, 49
 labels = [300, 150, 400, 500, 450, 550]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
-print(clf.predict([[3840, 55]]))
+quality = input("What's the quality of the tv?")
+size = input("What's the size of the tv?")
+print(clf.predict([[quality, size]]))
